3d5daa57f0a9d7fbfecb5aba777e21a859512057
DarkEleck <darkeleck.24@gmail.com>
2021-10-15 07:52:20 -0400

    Modify and add textures

--- src/main/java/com/darkeleck/coppergolem/entity/CopperGolemEntity.java
+++ src/main/java/com/darkeleck/coppergolem/entity/CopperGolemEntity.java
@@ -2,7 +2,6 @@ package com.darkeleck.coppergolem.entity;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.passive.GolemEntity;
 import net.minecraft.world.World;
 
 public class CopperGolemEntity extends PathAwareEntity {
